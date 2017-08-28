acbbb520c5a3efa63a3cc2f8ae7bb4599da00150
Intern <Seevil@users.noreply.github.com>
2017-08-29 01:00:21 +0800

    Update 3.0

--- conf.php
+++ conf.php
@@ -1,8 +1,7 @@
 <?php
 return array(
 	'name'=>'V2EX',
-	'user'=>'intern',
-	'mess'=>'V2EX',
-	'code' => 'hy_home_col',
-    'version' => '1.5',
+	'user'=>'Intern',
+	'mess'=>'仿V2EX社区模板',
+	'version' => '3.0',
 	);
